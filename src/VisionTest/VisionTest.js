c4a67fa529ca0bc97a78ca3f48d40e0203bfc289
Celeste A. Moore <77650238+celestealexmoore@users.noreply.github.com>
2023-05-12 20:37:45 -0500

    Switch statement to console.log which button was clicked.

--- src/VisionTest/VisionTest.js
+++ src/VisionTest/VisionTest.js
@@ -5,37 +5,63 @@ import optotypeD from "../optotypes/d.png";
 import optotypeS from "../optotypes/s.png";
 import "./VisionTest.css";
 
-const optotypes = {
-  H: optotypeH,
-  V: optotypeV,
-  Z: optotypeZ,
-  D: optotypeD,
-  S: optotypeS,
-};
-
-/*
-1. make 6 buttons with unique ids. 
-2. On click, show the corresponding letter
-3. when button is clicked, store what octotype was shown vs. what was selected.
-*/
-
-let generateRandom = function (obj) {
-  let keys = Object.keys(obj);
-  let randomIndex = Math.floor(Math.random() * keys.length);
-  return Object.values(obj)[randomIndex];
-};
-
 function VisionTest() {
+  const optotypes = {
+    H: optotypeH,
+    V: optotypeV,
+    Z: optotypeZ,
+    D: optotypeD,
+    S: optotypeS,
+  };
+
+  let generateRandom = function (obj) {
+    let keys = Object.keys(obj);
+    let randomIndex = Math.floor(Math.random() * keys.length);
+    return Object.values(obj)[randomIndex];
+  };
+
+  function whichBtn(btnId) {
+    switch (btnId) {
+      case "h":
+        console.log(`The ${btnId} was clicked.`);
+        break;
+      case "v":
+        console.log(`The ${btnId} was clicked.`);
+        break;
+      case "z":
+        console.log(`The ${btnId} was clicked.`);
+        break;
+      case "d":
+        console.log(`The ${btnId} was clicked.`);
+        break;
+      case "s":
+        console.log(`The ${btnId} was clicked.`);
+        break;
+      default:
+        console.log(`Sorry, that's not an option.`);
+    }
+  }
+
   return (
     <div className="visionTestParent">
       <h1 className="visionTestTitle">Vision Test</h1>
       <img src={generateRandom(optotypes)} className="letter" />
       <div className="buttonsParent">
-        <button className="btn">{Object.keys(optotypes)[0]}</button>
-        <button className="btn">{Object.keys(optotypes)[1]}</button>
-        <button className="btn">{Object.keys(optotypes)[2]}</button>
-        <button className="btn">{Object.keys(optotypes)[3]}</button>
-        <button className="btn">{Object.keys(optotypes)[4]}</button>
+        <button className="btn h" onClick={() => whichBtn("h")}>
+          {Object.keys(optotypes)[0]}
+        </button>
+        <button className="btn v" onClick={() => whichBtn("v")}>
+          {Object.keys(optotypes)[1]}
+        </button>
+        <button className="btn z" onClick={() => whichBtn("z")}>
+          {Object.keys(optotypes)[2]}
+        </button>
+        <button className="btn d" onClick={() => whichBtn("d")}>
+          {Object.keys(optotypes)[3]}
+        </button>
+        <button className="btn s" onClick={() => whichBtn("s")}>
+          {Object.keys(optotypes)[4]}
+        </button>
       </div>
     </div>
   );
